2eeedba92f24a1f2d350255737ce24ee5acf7a84
Tejas Dalvi <tejasdalvi.coep@gmail.com>
2024-06-18 14:04:50 +0530

    Environment created & installed libraries from requirements file.

--- init_setup.sh
+++ init_setup.sh
@@ -1,15 +1,12 @@
 echo [$(date)]: "START"
 
+echo [$(date)]: "creating environment with python 3.8 version" 
 
-echo [$(date)]: "creating env with python 3.8 version" 
-
-
-conda create --prefix ./env python=3.8 -y
-
+conda create --prefix ./gemstone python=3.8 -y
 
 echo [$(date)]: "activating the environment" 
 
-source activate ./env
+source activate ./gemstone
 
 echo [$(date)]: "installing the dev requirements" 
 
